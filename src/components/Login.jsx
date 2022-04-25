ef00d015cffa2c4dc1b0d3f7bfc281ac2b469db8
Graham Bragg <grahambragg@gmail.com>
2022-04-25 12:03:09 +1000

    Commit Anzac Day

---
+++ src/components/Login.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Login</h1>
+            <p>
+            Genealogy System Login
+            </p>
+            <p>
+              <b>
+                  Name:
+                  </b>
+            </p>
+            <p>
+           
+                  Password:
+           
+            </p>
+            <p>
+           
+                or 
+            </p>
+            <p>
+           
+              <i>
+                Login using Google:
+            
+                </i>
+            </p>
+            <p>
+           
+                Login using Facebook:
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
